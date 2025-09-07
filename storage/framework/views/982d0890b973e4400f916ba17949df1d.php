aa5c6c2dd45c777ff31e38208517a835da106d9c
mohamedelattaoui34 <mohamedlinkedin6@gmail.com>
2025-09-07 14:23:42 +1300

    hh

---
+++ storage/framework/views/982d0890b973e4400f916ba17949df1d.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>EduWeb - The Best Program to Enroll for Exchange</title>
+    <meta name="title" content="EduWeb - The Best Program to Enroll for Exchange">
+    <meta name="description" content="This is an education html template made by codewithsadee">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600;700;800&family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+
+    <!-- Preload Images -->
+    <link rel="preload" as="image" href="/assets/images/hero-bg.svg">
+    <link rel="preload" as="image" href="/assets/images/hero-banner-1.jpg">
+    <link rel="preload" as="image" href="/assets/images/hero-banner-2.jpg">
+    <link rel="preload" as="image" href="/assets/images/hero-shape-1.svg">
+    <link rel="preload" as="image" href="/assets/images/hero-shape-2.png">
+
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
+<body id="top">
+    <div id="app"></div>
+
+    <!-- IonIcons -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
+<?php /**PATH D:\education\my-app\resources\views/app.blade.php ENDPATH**/ ?>
